16961c62888aa6aac3b37449c4cc46ca7c2f674c
victorvicente <riolu1415@hotmail.com>
2020-07-25 14:28:26 -0500

    ComprobanteActu01

--- app/src/main/java/com/example/semana12b/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/semana12b/ui/gallery/GalleryFragment.java
@@ -1,9 +1,11 @@
 package com.example.semana12b.ui.gallery;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,6 +14,7 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.semana12b.ComprobanteActivity;
 import com.example.semana12b.R;
 
 public class GalleryFragment extends Fragment {
